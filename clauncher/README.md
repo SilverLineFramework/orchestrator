0ac76acb6ab00af007a634ff326c4a6ea2335ebb
Nuno Pereira <npereira@andrew.cmu.edu>
2020-08-17 10:35:18 -0400

    Removed clauncher

--- clauncher/README.md
+++
@@ -1,3 +0,0 @@
-# CONIX Launcher 
-
-TBD
--- clauncher/clauncher/settings.py
+++
@@ -1,8 +0,0 @@
-import json
-
-CFG_FILE = 'config.json'
-
-def load():
-    global s_dict
-    with open(CFG_FILE) as json_data_file:
-        s_dict = json.load(json_data_file)
