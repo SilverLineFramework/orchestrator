08db7734cbc86f86ee2f1b59b99e37954e471ab9
Nuno Pereira <nampereira@gmail.com>
2022-04-08 15:12:55 +0100

    feat: improved rr scheduler

--- arts-main/scheduler/rr.py
+++ arts-main/scheduler/rr.py
@@ -17,20 +17,40 @@ class RoundRobinScheduler(sb.SchedulerBase):
         print("New runtime: ", runtime_instance)
 
     @staticmethod
-    def schedule_new_module():
+    def schedule_new_module(module_instance):
         """
-        Returns the next runtime, ordered by updated date
-        everytime a new module is scheduled
+        Returns the next runtime, ordered by uuid
+        Performs basic checks (nmodules and apis) if the runtime can run the module
         """
         next_runtime = None
-        try:
-            next_runtime = Runtime.objects.order_by('uuid')[RoundRobinScheduler.next_index]
-            RoundRobinScheduler.next_index = RoundRobinScheduler.next_index + 1
-        except Exception:
-            RoundRobinScheduler.next_index = 0
-            next_runtime = Runtime.objects.order_by('uuid')[RoundRobinScheduler.next_index]
-            RoundRobinScheduler.next_index = RoundRobinScheduler.next_index + 1
-        #print('next:',next_runtime)
+        start_index = RoundRobinScheduler.next_index
+        while next_runtime == None:
+            try:
+                next_runtime = Runtime.objects.order_by('uuid')[RoundRobinScheduler.next_index]
+                RoundRobinScheduler.next_index = RoundRobinScheduler.next_index + 1
+            except Exception:
+                RoundRobinScheduler.next_index = 0
+                next_runtime = Runtime.objects.order_by('uuid')[RoundRobinScheduler.next_index]
+                RoundRobinScheduler.next_index = RoundRobinScheduler.next_index + 1
+            # next_runtime is our candidate runtime to run the module
+            if next_runtime:
+                # next_runtime can have more modules ?
+                if next_runtime.max_nmodules-next_runtime.nmodules > 0:
+                    # next_runtime supports apis needed by module ?
+                    for api in module_instance.apis:
+                        #print(api, 'in?', r.apis, api in r.apis)
+                        if (not api in next_runtime.apis):
+                            next_runtime = None
+                            break
+                else: next_runtime = None
+            # did we just try all runtimes ?
+            if start_index == RoundRobinScheduler.next_index:
+                next_runtime = None
+                break;
+
+        if (next_runtime == None):
+            raise Exception('No suitable runtime!')
+
         return next_runtime
 
     @staticmethod
